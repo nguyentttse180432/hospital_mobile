5555d307ba623309bbba351b8b235c8cce95d9f3
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2025-07-01 15:31:46 +0700

    add verfication api

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,16 +1,4 @@
-import api from './api';
-
-export const login = async (username, password) => {
-  try {
-    const response = await api.post('Auth/login', {
-      username,
-      password,
-    });
-    return response.data.value; 
-  } catch (error) {
-    throw error;
-  }
-};
+import api from './api';;
 
 export const loginWithGoogle = async (token) => {
   try {
@@ -22,3 +10,46 @@ export const loginWithGoogle = async (token) => {
     throw error;
   }
 };
+
+export const sendOtpToPhone = async (phoneNumber) => {
+  try {
+    const response = await api.post("Account/sms-sending", {
+      phoneNumber,
+    });
+    // {
+    //   "phoneNumber": "string"
+    // }
+    return response.data.value;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const verifyOtp = async (phoneNumber, code) => {
+  try {
+    const response = await api.post("Account/verification-sms", {
+      phoneNumber,
+      code,
+    });
+    // {
+    //   "phoneNumber": "string",
+    //   "code": "string"
+    // }
+    return response.data.value;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const updateAccount = async (data) => {
+  try {
+    const response = await api.put("Account", data);
+    // {
+    //   "accountId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    //   "phoneNumber": "stringstri"
+    // }
+    return response.data.value;
+  } catch (error) {
+    throw error;
+  }
+}
